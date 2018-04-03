0bd08ba7e833f8e9e502253ea6759b39cd603807
Felix Holz <felix.holz@me.com>
2018-04-03 19:20:40 +0200

    working on rate view

--- app/src/main/java/me/holz/ratemovies/AddRatingView.java
+++ app/src/main/java/me/holz/ratemovies/AddRatingView.java
@@ -5,9 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
+
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+import java.util.prefs.PreferencesFactory;
 
 public class AddRatingView extends AppCompatActivity {
 
@@ -18,10 +26,31 @@ public class AddRatingView extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final EditText nametext = findViewById(R.id.nametext);
+        Button submitbutton = findViewById(R.id.button);
+        final Preferences prefs = Preferences.userRoot();
+
+        nametext.setText(prefs.get("name", ""));
+
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prefs.put("name", nametext.getText().toString());
+                try {
+                    prefs.flush();
+                } catch (BackingStoreException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
 
         RatingBar ratingbar = findViewById(R.id.ratingBar2);
 
         setTitle(getIntent().getExtras().getString("movie"));
+
+
+
         /* Back Button crashes app when pressed, tempoarily disabled */
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
